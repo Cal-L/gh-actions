f014e7cb7fee9e126b0f0edbb4764cd60b19c3d4
Cal Leung <cleun007@gmail.com>
2023-05-04 20:13:04 -0700

    add

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("HELLO");
+import core from "@actions/core";
+import github from "@actions/github";
+
+const token = process.env.GITHUB_TOKEN as string;
+const octokit = github.getOctokit(token);
+
+console.log("token", token[0]);
