417502c93751a5db17a1d04babe556566bcb2a2f
Cal Leung <cleun007@gmail.com>
2023-05-04 20:28:06 -0700

    k

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+const prTemplatePath = ".github/PULL_REQUEST_TEMPLATE.md";
+const context = github.context;
 const token = process.env.GITHUB_TOKEN;
 const octokit = github.getOctokit(token);
+
+console.log("Octo", octokit.repos);
 
 console.log("token", token[0]);
