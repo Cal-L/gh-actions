289ff29f94b71b16d06a419f731abf30ef0b8e48
Cal Leung <cleun007@gmail.com>
2023-05-04 20:22:32 -0700

    k

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-const token = process.env.GITHUB_TOKEN as string;
+const token = process.env.GITHUB_TOKEN;
 const octokit = github.getOctokit(token);
 
 console.log("token", token[0]);
