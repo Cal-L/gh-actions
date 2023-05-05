cf97c8eed566f2b342c404187ad1884431e80171
Cal Leung <cleun007@gmail.com>
2023-05-05 08:37:44 -0700

    k

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-// import core from "@actions/core";
-// import github from "@actions/github";
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-const prTemplatePath = ".github/PULL_REQUEST_TEMPLATE.md";
-const token = process.env.GITHUB_TOKEN;
+const githubToken = process.env.GITHUB_TOKEN;
+const octokit = github.getOctokit(githubToken);
 
-const context = github.context;
-const title = context.payload.pull_request.title;
-const branch = context.payload.pull_request.head.ref;
+const githubContext = github.context;
+const pullRequestTitle = context.payload.pull_request.title;
+const headBranchName = context.payload.pull_request.head.ref;
 const owner = context.repo.owner;
 const repo = context.repo.repo;
-const octokit = github.getOctokit(token);
 
 /**
  * Checks if PR title is valid.
@@ -21,73 +18,20 @@ const octokit = github.getOctokit(token);
  */
 const checkTitle = async () => {
   try {
-    // const repoResponse = await octokit.rest.repos.getContent({
-    //   owner,
-    //   repo,
-    //   path: prTemplatePath,
-    // });
-    // const fileUrl = await repoResponse.data.download_url;
-    // const contentResponse = await fetch(fileUrl);
-    // const text = await contentResponse.text();
-
+    throw new Error("#WHAM");
     const validPrefixes = ["[FEATURE]", "[FIX]", "[IMPROVEMENT]"];
     for (const prefix of validPrefixes) {
       if (title.startsWith(prefix)) {
-        core.info("Title format looks goods!");
+        core.info("Title format looks good!");
         return;
       }
     }
     core.setFailed(
       "Title is not a valid syntax. It needs to be prefixed with one of the following - [FEATURE], [FIX], or [IMPROVEMENT]."
     );
-  } catch (err) {
-    console.log("Failed to validate title.", err);
+  } catch (error) {
+    core.warning(`Title validation failed with error ${error}.`);
   }
 };
 
-const freezeMerge = async () => {
-  try {
-    await octokit.rest.repos.updateBranchProtection({
-      owner,
-      repo,
-      branch,
-      required_status_checks: {
-        strict: false,
-      },
-      enforce_admins: true,
-      required_pull_request_reviews: null,
-      restrictions: null,
-      allow_deletions: true,
-    });
-  } catch (err) {
-    console.log("Failed to freeze merge", err);
-  }
-};
-
-const enableMerge = async () => {
-  try {
-    await octokit.repos.updateBranchProtection({
-      owner,
-      repo,
-      branch,
-      required_status_checks: {
-        strict: true,
-      },
-      enforce_admins: true,
-      required_pull_request_reviews: null,
-      restrictions: null,
-      allow_deletions: true,
-    });
-  } catch (err) {
-    console.log("Failed to enable merge.", err);
-  }
-};
-
-const main = async () => {
-  //   await freezeMerge();
-  await checkTitle();
-  //   await enableMerge();
-  // const fileUrl = await response.data.download_url;
-};
-
-main();
+checkTitle();
