c1b65607474acf8cb4f33e95bcc54514302eb483
Cal Leung <cleun007@gmail.com>
2023-05-04 20:37:45 -0700

    k

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 const prTemplatePath = ".github/PULL_REQUEST_TEMPLATE.md";
-const context = github.context;
 const token = process.env.GITHUB_TOKEN;
 const octokit = github.getOctokit(token);
 
-console.log("Octo", octokit.repos);
+const context = github.context;
 
-console.log("token", token[0]);
+const main = async () => {
+  const owner = context.repo.owner;
+  const repo = context.repo.repo;
+  const response = await octokit.rest.repos.getContent({
+    owner,
+    repo,
+    path: prTemplatePath,
+  });
+  const k = response.data;
+  console.log("LOL", k);
+  // const fileUrl = await response.data.download_url;
+};
+main();
