199392ed8cc4501b7ef89dcd8512f02a76571e64
Cal Leung <cleun007@gmail.com>
2023-05-04 21:20:49 -0700

    k

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const token = process.env.GITHUB_TOKEN;
 const octokit = github.getOctokit(token);
 
 const context = github.context;
-const title = context.payload.pull_request.title;
+const title = context.payload;
 
 const main = async () => {
   const owner = context.repo.owner;
