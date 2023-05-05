0e53685447ea44fa99a5f28a020e38f4f0f54c79
Cal Leung <cleun007@gmail.com>
2023-05-04 20:17:20 -0700

    a

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
-import core from "@actions/core";
-import github from "@actions/github";
+// import core from "@actions/core";
+// import github from "@actions/github";
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 const token = process.env.GITHUB_TOKEN as string;
 const octokit = github.getOctokit(token);
