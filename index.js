79a16807a74c691be5498a1088ceeb9e17eb8f95
Cal Leung <cleun007@gmail.com>
2023-05-04 21:30:58 -0700

    k

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// import core from "@actions/core";
-// import github from "@actions/github";
-const core = require("@actions/core");
-const github = require("@actions/github");
+import core from "@actions/core";
+import github from "@actions/github";
+// const core = require("@actions/core");
+// const github = require("@actions/github");
 
 const prTemplatePath = ".github/PULL_REQUEST_TEMPLATE.md";
 const token = process.env.GITHUB_TOKEN;
 const octokit = github.getOctokit(token);
 
 const context = github.context;
-const title = github.event.pull_request.title;
+const title = context.payload.pull_request.title;
 
 const main = async () => {
   const owner = context.repo.owner;
